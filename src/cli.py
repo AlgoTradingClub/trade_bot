fbd2c0c3c66bba515813d20fd7a8e506d1deae41
Bryson <bryson.meiling@aggiemail.usu.edu>
2021-02-03 19:04:54 -0700

    added a cli file that I want to act like manage.py from django where it can be the cli entry point into the entire system

---
+++ src/cli.py
@@ -0,0 +1,8 @@
+'''
+Here we could make a cli interface file that works similar to 'manage.py' from django. Meaning, that since this is in the top level directory, it can run any function, unittest, and would make a centralized point of entry for any of the programs.
+
+Does anyone have objection for this?
+
+This file could use the package pyCLI to automate the command line interface making process.
+maybe using 'click' or 'fire'. They seem a bit more friendly than 'argparse'
+'''
