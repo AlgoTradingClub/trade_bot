bc88a1371366be18da3dc3f64ba2c4b3bc65fcf3
Bryson <bryson.meiling@gmail.com>
2021-09-20 16:36:37 -0400

    starting find pairs function

--- utils/Find_Pairs.py
+++ utils/Find_Pairs.py
@@ -5,10 +5,11 @@ import pathlib as p
 
 
 def find_pairs(timespan: int = 365):
-    data_dir = p.Path(__file__).parent.parent.joinpath("data")
-    pairs_data = data_dir.joinpath("pairs_data.txt")
+    today = str(datetime.now().date())
+    data_dir = p.Path(__file__).parent.parent.joinpath("data").joinpath("pairs_calculations")
+    pairs_data = data_dir.joinpath(f"{today}_pairs_data.txt")
     account = AlpacaAccount()
     api = AlpacaData()
     assets = account.list_assets(shortable=True, fractionable=True)
 
-    print(f"Calculation completed. Data saved in {data_dir}")
+    print(f"Calculation completed. Data saved in {pairs_data}")
